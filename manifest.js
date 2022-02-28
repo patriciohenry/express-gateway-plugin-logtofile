320b132a66e88f8583109e5ff88f130554f79fe5
patriciohenry <patricio.gajardo@kiwox.cl>
2022-02-28 08:36:35 -0300

    inicial

--- manifest.js
+++ manifest.js
@@ -5,7 +5,7 @@
 module.exports = {
   version: '1.2.0',
   init: function (pluginContext) {
-    pluginContext.registerPolicy(require('express-gateway-plugin-logger/policies/filelog-policy'));
+    pluginContext.registerPolicy(require('express-gateway-plugin-logtofile/policies/filelog-policy'));
 
   },
   policies: ['filelog'] 
